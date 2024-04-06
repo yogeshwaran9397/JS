f3bfbcbe54d742ae46499a082d3575c99414c4fc
Yogesh <yogesh@graniteriverlabs.in>
2024-04-06 22:56:22 +0530

    Fundamentals Part 1

---
+++ Fundamental 1/script.js
@@ -0,0 +1,42 @@
+// alert("Hello")
+
+const a = "yogesh";
+// a ="Yogesh1"
+console.log(a);
+
+let b = 1;
+console.log(b);
+
+const name = "Yogesh";
+const age = 20 ;
+const stringDesc = `I'm ${name} my age is ${age}`;
+console.log(stringDesc);
+
+//If and Else 
+const num = 10;
+if(num <10)
+    console.log("number is lesser than 10")
+else if(num >10)
+    console.log("number is greater than 10")
+else
+    console.log("number is == 10")
+
+console.log(typeof name);
+console.log(typeof a);
+console.log(typeof b);
+
+//Ternary
+const personage = 18 ;
+personage >=18? 
+console.log("Person can drink 👍") : 
+console.log("Person should not drink 🚫");
+
+
+const Yes = true;
+const No = false;
+console.log(Yes && Yes);
+console.log(Yes && No);
+
+
+
+
