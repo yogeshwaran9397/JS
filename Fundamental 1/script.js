07d6623cf05eef1d2371d9a10d8f0bd77b99baf4
Yogesh <yogesh@graniteriverlabs.in>
2024-04-17 11:10:13 +0530

    Fundamental 2 JS

--- Fundamental 1/script.js
+++ Fundamental 1/script.js
@@ -1,35 +1,38 @@
 // alert("Hello")
 
 const a = "yogesh";
-// a ="Yogesh1"
+// a ="Yogesh1" //Error
 console.log(a);
 
 let b = 1;
 console.log(b);
 
 const name = "Yogesh";
-const age = 20 ;
+const age = 20;
 const stringDesc = `I'm ${name} my age is ${age}`;
 console.log(stringDesc);
 
 //If and Else 
 const num = 10;
-if(num <10)
+if (num < 10)
     console.log("number is lesser than 10")
-else if(num >10)
+else if (num > 10)
     console.log("number is greater than 10")
 else
     console.log("number is == 10")
 
+const unDef = undefined;
+
 console.log(typeof name);
 console.log(typeof a);
 console.log(typeof b);
+console.log(typeof unDef);
 
 //Ternary
-const personage = 18 ;
-personage >=18? 
-console.log("Person can drink 👍") : 
-console.log("Person should not drink 🚫");
+const personage = 18;
+personage >= 18 ?
+    console.log("Person can drink 👍") :
+    console.log("Person should not drink 🚫");
 
 
 const Yes = true;
